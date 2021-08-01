089296531bac9edbf39cea07df7881dbe7cda0c3
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-01 14:13:23 -0700

    Require cors module

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan("common"));
 
+const cors = require('cors');
+app.use(cors());
+
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
