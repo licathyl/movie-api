8642eb0978e518b7d84f874f449be0aef902f176
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-15 13:50:35 -0700

    Remove authentication for /movies endpoint

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ app.get("/", (req, res) => {
 });
 
 // Gets a list of all movies
-app.get("/movies", passport.authenticate("jwt", { session: false }), (req, res) => {
+app.get("/movies", function (req, res) {
   Movies.find()
-    .then((movies) => {
+    .then(function (movies) {
       res.status(201).json(movies);
     })
-    .catch((err) => {
+    .catch(function (err) {
       console.error(err);
       res.status(500).send("Error: " + err);
     });
