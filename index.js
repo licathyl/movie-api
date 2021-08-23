1c6ab7c157a6e226b227cd76144536539b5c1407
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-23 14:48:02 -0700

    Update /movies endpoint

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 // Gets a list of all movies
-app.get("/movies", function (req, res) {
+app.get("/movies", passport.authenticate("jwt", { session: false }), (req, res) => {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies);
